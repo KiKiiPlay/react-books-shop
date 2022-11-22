a674db34a588b34e2490bfb8e9ff7f308698328a
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2022-11-23 00:24:47 +0100

    Start of creation Welcome component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import MyNav from "./component/MyNav";
 import Myfooter from "./component/MyFooter";
 
 function App() {
   return (
-    <div>
-      <MyNav subtitle="Strive-Shop" />
-      <Myfooter />
-    </div>
+    <Container fluid>
+      <Row className="d-flex flex-column">
+        <Col>
+          <MyNav subtitle="Strive-Shop" />
+        </Col>
+        <Col>
+          <Myfooter />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
